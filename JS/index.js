dd8d6c208f66b2d8dc068ee67797db57e33ffd39
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-17 13:41:17 +0100

    calcoato eta e kilometri

--- JS/index.js
+++ JS/index.js
@@ -5,3 +5,14 @@
 // va applicato uno sconto del 40% per gli over 65.
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 // Questo richiederà un minimo di ricerca.
+
+// chiedere all'utente i chilometri da percorrere e la sua età 
+let lungezzaTragitto = parseInt(prompt("quanti chiloetri vuoi percorrere"));
+console.log(lungezzaTragitto);
+
+let age= parseInt(prompt("quanti anni hai "));
+console.log(age);
+// calcolare prezzo biglietto
+
+// applicare sconto under18 e over 65 
+// scrivere output prezzo in decimale 
